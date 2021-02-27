937355637e4c1426eac6041510ac940bffb60d92
Nemanja <nemanjadragomirovic086@gmail.com>
2021-02-27 10:57:27 +0100

    Implement Java 1.2 - Einfache Rechenaufgaben

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b;
+        a=138;
+        b=235;
+
+
+        System.out.println(a + "+" + b + "=" + (a+b));
+        System.out.println(a + "-" + b + "=" + (a-b));
+        System.out.println(b + "/" + a + "=" + (a/b));
+        System.out.println(b + "*" + a + "=" + (a*b));
+    }
+}
